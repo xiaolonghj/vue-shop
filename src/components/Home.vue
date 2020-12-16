8a8c679843cca5f1f01ff736590e2143ef1cb496
xiaolonghj <2536095958@qq.com>
2020-12-16 10:31:00 +0800

    登录功能完成

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <el-button type="info" @click="loginOut">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 实现退出登录的方法
+    loginOut () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.home {
+  text-align: left;
+  margin: 30px 0 0 30px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,19 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
-Vue.config.productionTip = false
+/* 全局导入ElementUI */
+import ElementUI from 'element-ui'
+/* 全局样式 */
+import './assets/css/global.css'
+/* 引入axios */
+import axios from 'axios'
+/* 配置根路径 */
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
 new Vue({
   router,
   store,
